1a2f0905896edbd275fd8e836f7ce19bbb8856ce
Elise Brenna <elisebrenna@gmail.com>
2022-10-18 06:45:34 +0200

    .

--- src/About.js
+++ src/About.js
@@ -10,16 +10,14 @@ const About = ({ textEnter, textLeave, onMouseEnter, onMouseLeave }) => {
   return (
     <StyledContainer>
       <Content>
-        <ImgContainer>
-        {/* <img src={BeachSrc} /> */}
-        </ImgContainer>
+        <ImgContainer>{/* <img src={BeachSrc} /> */}</ImgContainer>
         <Text>
           <h2>Formidling av eiendom på solens kyst</h2>
           <p>
-          <p>
-            Ja, med 306 soldager i året velger vi å kalle Spanias østkyst
-            nettopp dette.
-          </p>
+            <p>
+              Ja, med 306 soldager i året velger vi å kalle Spanias østkyst
+              nettopp dette.
+            </p>
             Vi er et norsk selskap som samarbeider med{" "}
             <a
               href="https://mojacarestates.com/"
@@ -30,13 +28,14 @@ const About = ({ textEnter, textLeave, onMouseEnter, onMouseLeave }) => {
               Mojacar Estates
             </a>
             , en av de mest solide eiendomsmeglere og utbyggere i
-            Andalusía/Almería-regionen i Spania. Porteføljen inneholder også
-            småbruk og mulighet for kjøp av tomt. Innehaver av Sunrise Homes har
-            forøvrig hatt egen leilighet i Mojacar siden 2003 og kjenner derfor
-            området meget godt. Med vår eksklusive formidlingsavtale med Mojacar
-            Estates ønsker vi norske kunder hjertelig velkommen, da dette
-            fantastiske området som kalles "the real Spain" og "a place in the
-            sun" bare må oppleves.
+            Andalusía/Almería-regionen i Spania. Mojacar Estates tilbyr boliger
+            for kjøp og leie – både kort og langtidsutleie. Porteføljen
+            inneholder også småbruk og mulighet for kjøp av tomt. Innehaver av
+            Sunrise Homes har forøvrig hatt egen leilighet i Mojacar siden 2003
+            og kjenner derfor området meget godt. Med vår eksklusive
+            formidlingsavtale med Mojacar Estates ønsker vi norske kunder
+            hjertelig velkommen, da dette fantastiske området som kalles "<em>the
+            real Spain</em>" og "<em>a place in the sun</em>" bare må oppleves.
           </p>
         </Text>
       </Content>
